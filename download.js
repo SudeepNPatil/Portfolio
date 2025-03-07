e352b5611750f0a16f126eb8121e37bc7659a519
SudeepNPatil <sudeepbsc02@gmail.com>
2025-03-07 20:07:43 +0530

    miner update

--- download.js
+++ download.js
@@ -1,31 +1,16 @@
-const down = document.getElementById("download1").addEventListener("click", function () {
 
-    const link = document.createElement("a")
 
-    link.href = "./SUDEEP N PATIL.CV.pdf";
-    link.download = "Sudeep.cv.pdf";
-
-    document.body.appendChild(link);
-    link.click();
-    document.body.removeChild(link);
-
-});
 
 // Ensure JavaScript runs after DOM is loaded
 document.addEventListener("DOMContentLoaded", function some() {
+
+
     // Select elements AFTER the page has loaded
     const popup = document.getElementById("popup");
     const closeBtn = document.getElementById("close-btn");
 
     // Function to open popup
-    function openPopup() {
-        popup.style.display = "flex";
-    }
 
-    // Function to close popup
-    function closePopup() {
-        popup.style.display = "none";
-    }
 
     // Add event listener for close button (if it exists)
     if (closeBtn) {
